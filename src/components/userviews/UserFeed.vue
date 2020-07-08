2b4230bae7287d1c4d332fd8c689599601499c96
disgraceful <noolic97@gmail.com>
2020-07-08 19:27:13 +0300

    Few unit tests

--- src/components/userviews/UserFeed.vue
+++ src/components/userviews/UserFeed.vue
@@ -2,10 +2,29 @@
   <v-card flat>
     <v-container>
       <v-card-title class="py-0">User Feed</v-card-title>
+      <bs-user-record v-for="index in records" :key="index" :record="index"></bs-user-record>
     </v-container>
   </v-card>
 </template>
 
 <script>
-export default {};
+import UserRecord from "./UserRecord.vue";
+export default {
+  components: { "bs-user-record": UserRecord },
+  data() {
+    return {
+      records: [
+        {
+          date: "01/02/2020",
+          text: "Finished Konan the Barbarian",
+          link: "/book/12345"
+        },
+        1,
+        2,
+        3,
+        4
+      ]
+    };
+  }
+};
 </script>
