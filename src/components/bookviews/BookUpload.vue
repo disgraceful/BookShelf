2d9a3c7816c719db98ea18343b0a2112c9bdf548
disgraceful <noolic97@gmail.com>
2020-07-30 18:46:01 +0300

    uploading book cover with form data

--- src/components/bookviews/BookUpload.vue
+++ src/components/bookviews/BookUpload.vue
@@ -4,7 +4,7 @@
       <v-card>
         <v-container class="px-7">
           <v-card-title class="px-0 text-uppercase">Upload private book</v-card-title>
-          <v-form lazy-validation v-model="valid">
+          <v-form ref="form" v-model="valid" lazy-validation>
             <v-row justify="start">
               <v-col cols="auto">
                 <v-img :src="imgPath" max-height="200px" width="150px" contain></v-img>
@@ -18,11 +18,12 @@
               :rules="imgRules"
               accept="image/*"
             ></v-file-input>
-            <v-text-field label="Title" clearable required :rules="titleRules"></v-text-field>
-            <v-text-field label="Author" clearable required :rules="authorRules"></v-text-field>
-            <v-text-field label="Pages" clearable required :rules="pagesRules" type="number"></v-text-field>
+            <v-text-field label="Title" v-model="title" :rules="titleRules"></v-text-field>
+            <v-text-field label="Author" v-model="author" :rules="authorRules"></v-text-field>
+            <v-text-field label="Pages" v-model="pages" :rules="pagesRules" type="number"></v-text-field>
             <v-textarea
               label="Book description"
+              v-model.number="description"
               clearable
               rows="1"
               counter="200"
@@ -34,7 +35,7 @@
                 <v-btn>Cancel</v-btn>
               </v-col>
               <v-col cols="auto">
-                <v-btn :disabled="!valid">Upload</v-btn>
+                <v-btn :disabled="!formValid" @click="upload">Upload</v-btn>
               </v-col>
             </v-row>
           </v-form>
@@ -45,11 +46,16 @@
 </template>
 
 <script>
+import uploadService from "../../services/uploadService";
 export default {
   data() {
     return {
       dialog: true,
-      valid: true,
+      valid: false,
+      title: "",
+      author: "",
+      pages: "",
+      description: "",
       cover: null,
       defaultImg: require("../../assets/goodreads.png"),
       imgRules: [
@@ -80,6 +86,22 @@ export default {
       return this.cover && this.cover.size > 0
         ? URL.createObjectURL(this.cover)
         : this.defaultImg;
+    },
+    formValid() {
+      return (
+        this.title !== "" && this.author !== "" && +this.pages > 0 && this.valid
+      );
+    }
+  },
+  methods: {
+    upload() {
+      let data = new FormData();
+      data.append("title", this.title);
+      data.append("author", this.author);
+      data.append("pages", this.pages);
+      data.append("description", this.description);
+      data.append("cover", this.cover || this.defaultImg);
+      uploadService.uploadBook(data).then(response => console.log(response));
     }
   }
 };
