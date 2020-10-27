2378de7493a1892ae2fc50ff7099c675ca2fb8cd
disgraceful <noolic97@gmail.com>
2020-10-27 16:47:09 +0200

    prod build

--- src/http/config.js
+++ src/http/config.js
@@ -3,6 +3,6 @@ import VueResource from "vue-resource";
 import interceptor from "./interceptor";
 
 Vue.use(VueResource);
-Vue.http.options.root = "http://localhost:4200/";
+Vue.http.options.root = "https://bookshelf-server.herokuapp.com/";
 
 Vue.http.interceptors.push(interceptor);
