00b31c4a7862bb3bbf12093a9f981b2d9e778fb8
disgraceful <noolic97@gmail.com>
2020-11-12 15:41:53 +0200

    backend root for prod and dev add

--- src/http/config.js
+++ src/http/config.js
@@ -3,6 +3,10 @@ import VueResource from "vue-resource";
 import interceptor from "./interceptor";
 
 Vue.use(VueResource);
-Vue.http.options.root = "https://bookshelf-server.herokuapp.com/";
+Vue.http.options.root =
+  process.env.NODE_ENV === "production"
+    ? process.env.VUE_APP_SERVER_PROD
+    : process.env.VUE_APP_SERVER_DEV;
 
+console.log(Vue.http.options.root);
 Vue.http.interceptors.push(interceptor);
